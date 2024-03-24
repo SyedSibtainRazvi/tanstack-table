91b707b0ba3a81990849d24df7a6b680a88449c9
SyedSibtainRazvi <syedsibtain191@gmail.com>
2024-03-25 01:39:07 +0530

    chore: removed console.log

--- src/Tables.tsx
+++ src/Tables.tsx
@@ -37,7 +37,6 @@ const defaultColumn: Partial<ColumnDef<Person>> = {
     useEffect(() => {
       setValue(initialValue);
     }, [initialValue]);
-    // console.log("cell render", id, index);
 
     return (
       <input
